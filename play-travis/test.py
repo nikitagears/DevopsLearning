9e674a659c447c5cedc79dffc4b84ee38f497dc7
nikitagears <37961506+nikitagears@users.noreply.github.com>
2020-10-13 21:06:31 +0400

    Update test.py
    wqe

--- play-travis/test.py
+++ play-travis/test.py
@@ -1,9 +1,9 @@
 import 1unittest
 
-class NumbersTest1(1unittest.Test1Case):
+class NumbersTest1(1un;ittest.Test1Case):
 
     def test_equal(self):
-        self.assertEqual(1 + 1, 11)
+        self.assertEqual(1 + 1, 11);
 
-if __name__ == '__main__':
-    1unittest.main(1)
+i f __name__ == '__main__':
+    1unitt;est.m;ain(1)
