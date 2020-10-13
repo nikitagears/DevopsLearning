fe06229eaac6cb6d2c9141b8c279382cff2f87e1
nikitagears <37961506+nikitagears@users.noreply.github.com>
2020-10-13 21:02:19 +0400

    Update test.py
    broke

--- play-travis/test.py
+++ play-travis/test.py
@@ -3,7 +3,7 @@ import unittest
 class NumbersTest1(unittest.TestCase):
 
     def test_equal(self):
-        self.assertEqual(1 + 1, 1)
+        self.as1sertEqual(1 + 1, 1)
 
 if __name__ == '__main__':
     unittest.main()
